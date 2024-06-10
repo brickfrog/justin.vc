4bf9fb87de2be27bea82282a08dc001c7379b846
Justin <9146678+brickfrog@users.noreply.github.com>
2024-06-10 11:18:02 -0400

    fixing opml

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# quotebacks trimming
 jq 'map(.quotes |= map(del(.articleText)))' ./static/quotes.json > ./static/modified_quotes.json
+
+# elfeed movement
+cp ~/.doom.d/elfeed.opml ./static/feed.opml
