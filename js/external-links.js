fcee1611664c7235dfcee4d9fc2bc05074ba1995
brickfrog <brickfrog@users.noreply.github.com>
2024-06-20 11:41:35 +0000

    deploy: 858dc0047221c0926c97c4b6d66eb69c684cd36b

---
+++ js/external-links.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var links = document.querySelectorAll("a[href^='http']");
+  var internalDomain = "justin.vc"; // replace with your actual domain
+
+  links.forEach(function (link) {
+    if (
+      link.hostname !== window.location.hostname &&
+      link.hostname !== internalDomain &&
+      !link.querySelector("svg") &&
+      !link.classList.contains("no-external-icon") &&
+      !link.closest(".meta-item") &&
+      !link.closest(".footer")
+    ) {
+      var iconClass = "fa-external-link-alt"; // default icon
+      if (link.hostname.includes("wikipedia.org")) {
+        iconClass = "fa-wikipedia-w";
+      }
+      if (link.hostname.includes("github.com")) {
+        iconClass = "fa-github";
+      }
+      // add more conditions for other domains here
+
+      link.classList.add("external-link");
+      link.setAttribute("data-icon", iconClass);
+    }
+  });
+});
