ab337872f080b085382d4e069c514810ecbdc19c
Justin <9146678+brickfrog@users.noreply.github.com>
2026-02-05 13:25:00 -0500

    feat: replace hugo with static site
    ditched the entire hugo build pipeline for a single index.html.
    CRT terminal aesthetic with forest background, collapsible
    background/media sections, anilist + goodreads + spotify embeds.

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# quotebacks trimming
-jq 'map(.quotes |= map(del(.articleText)))' ./static/quotes.json > ./assets/modified_quotes.json
-
-# elfeed movement
-cp ~/.doom.d/elfeed.opml ./static/feed.opml
